ec027ae9bba7ecc940a15a844dd28a74da70a851
ahsansaif47@ <ahsansaif47@hotmail.com>
2022-07-13 02:11:54 +0500

    variables basics

---
+++ variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age uint = 23
+	var name string = "Ahsan"
+	fmt.Println(name)
+	name = "Ahsan Saif"
+	fmt.Println("Name is: ", name, "Age is: ", age)
+}
