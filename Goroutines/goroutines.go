2a58ee0ca7460533091347ae0f7191c6fabaad25
ahsansaif47@ <ahsansaif47@hotmail.com>
2022-07-29 01:14:56 +0500

    goroutines

---
+++ Goroutines/goroutines.go
@@ -0,0 +1,44 @@
+package goroutines
+
+func main() {
+	// Simple GoRoutiens with time.Sleep()
+	// go_routines()
+
+	// Channels basic
+	// channels_basics()
+
+	// Channels with capacity
+	// channel_capacity()
+
+	// WaitGropus
+	// wg_basic()
+
+	// Mutexes and RWMutexes
+	// Mutex_practice()
+	// RWMutex_practice()
+
+}
+
+func Mutex_practice() {
+	panic("unimplemented")
+}
+
+func RWMutex_practice() {
+	panic("unimplemented")
+}
+
+func wg_basic() {
+	panic("unimplemented")
+}
+
+func channel_capacity() {
+	panic("unimplemented")
+}
+
+func channels_basics() {
+	panic("unimplemented")
+}
+
+func go_routines() {
+	panic("unimplemented")
+}
